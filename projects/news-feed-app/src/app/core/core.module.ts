5c8f1cd635d3b311dc64dbf86d6963adb559eb18
tetra <info.golab@gmail.com>
2021-04-30 19:56:32 +0400

    add core module

---
+++ projects/news-feed-app/src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+/**
+ * 
+ * CoreModule just containe global singletons so the application start up time will be faster
+ *
+ */
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    MainLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+
+    // material
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  exports:[MainLayoutComponent]
+})
+export class CoreModule { }
